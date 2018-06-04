5fc8e382a6f07f83939642cdd87f893d0b5ee1a8
常思安 <464679144@qq.com>
2018-06-04 15:54:05 +0800

    no message

--- service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
@@ -1,10 +1,13 @@
 package com.example.serviceribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @SpringBootApplication
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class ServiceRibbonApplication {
 
 	public static void main(String[] args) {
@@ -29,7 +34,11 @@ public class ServiceRibbonApplication {
     RestTemplate restTemplate;
 
     @RequestMapping(value = "/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hi(@RequestParam String name){
-        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+        return restTemplate.getForObject("http://service-hi/hi?name="+name,String.class);
+    }
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
